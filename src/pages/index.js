423bab39248679ae0f31fa98e0e1f88eec35bb9c
milescunningham <miles5cham@gmail.com>
2022-06-17 23:03:43 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,18 @@
 // Step 1: Import React
 import * as React from "react"
+import Layout from "../components/layout"
 import { Link } from 'gatsby'
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <main>
+      <Layout>
       <title>My Personal Portfolio</title>
       <h1>Welcome to my website.</h1>
       <p>This is my homepage.</p>
       <Link to="/about">About</Link>
+      </Layout>
     </main>
   )
 }
