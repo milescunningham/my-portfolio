a7ca539861a6a7b03c7c540e30c1217a9a18383d
milescunningham <miles5cham@gmail.com>
2022-06-17 21:04:48 -0400

    Create about.js

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+// Step 1: Import React
+import * as React from 'react'
+
+// Step 2: Define your component
+const AboutPage = () => {
+    return (
+        <main>
+            <title>Miles Cunningham</title>
+            <h1>About Me</h1>
+            <p>Miles Cunningham is from Little Silver, New Jersey and is a student at the University of South Carolina in Columbia, South Carolina. He is currently enrolled in the College of Undergraduate Studies as he looks to determine what course of study he would like to pursue.</p>
+        </main>
+    )
+}
+// Step 3: Export your component
+export default AboutPage
