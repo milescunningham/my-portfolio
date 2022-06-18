cc31ea7df3d2fc7581d010a72e7319c01dc9e41d
milescunningham <miles5cham@gmail.com>
2022-06-17 23:38:01 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,27 +9,27 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `pages`,
-        path: `${__dirname}/src/pages/`
+        path: `${__dirname}/src/pages`
       },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `posts`,
-        path: `${__dirname}/src/posts/`
+        path: `${__dirname}/src/posts`
       },
     },{
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images/`
+        path: `${__dirname}/src/images`
       },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `components`,
-        path: `${__dirname}/src/components/`
+        path: `${__dirname}/src/components`
       },
     },
     "gatsby-plugin-image",
