a0e9fe57102ac6511701b8e1e13f86caea89ea8c
milescunningham <miles5cham@gmail.com>
2022-06-17 21:11:42 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 // Step 1: Import React
 import * as React from "react"
+import { Link } from 'gatsby'
 
 // Step 2: Define your component
 const IndexPage = () => {
@@ -8,6 +9,7 @@ const IndexPage = () => {
       <title>My Personal Portfolio</title>
       <h1>Welcome to my website.</h1>
       <p>This is my homepage.</p>
+      <Link to="/about">About</Link>
     </main>
   )
 }
