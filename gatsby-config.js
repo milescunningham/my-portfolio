c832493bf839af4827c94f807fc8a7402f661c00
milescunningham <miles5cham@gmail.com>
2022-06-17 21:44:48 -0400

    plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,31 @@
 module.exports = {
+
   siteMetadata: {
     title: `my-portfolio`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts`
+      },
+    },{
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`
+      },
+    },
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
