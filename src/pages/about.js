f927b7b40a90024e385c93344fbd18c12d177c8f
milescunningham <miles5cham@gmail.com>
2022-06-17 21:15:31 -0400

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,6 @@
 // Step 1: Import React
 import * as React from 'react'
+import { Link } from 'gatsby'
 
 // Step 2: Define your component
 const AboutPage = () => {
@@ -8,6 +9,7 @@ const AboutPage = () => {
             <title>Miles Cunningham</title>
             <h1>About Me</h1>
             <p>Miles Cunningham is from Little Silver, New Jersey and is a student at the University of South Carolina in Columbia, South Carolina. He is currently enrolled in the College of Undergraduate Studies as he looks to determine what course of study he would like to pursue.</p>
+            <Link to="/index">Home</Link>
         </main>
     )
 }
